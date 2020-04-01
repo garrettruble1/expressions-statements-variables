28d71b72d8896ef9c5520136a88720e0b7817a74
“garrettruble1” <garrettruble1@gmail.com>
2020-04-01 15:57:32 -0400

    Operators practice

--- global.js
+++ global.js
@@ -8,43 +8,45 @@ console.log(4 + 5)
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here 
-console.log('answer 1');
+console.log(3 + 4);
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here 
-console.log('answer 2');
+console.log(4 - 1);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here 
-console.log('answer 3');
+console.log("I'm writing" + " " + "my first JavaScript code");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here 
-console.log('answer 4');
+console.log(7 > 8);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log('answer 5');
+console.log(13 % 9);
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 6.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6');
+// your code here
+var x = "hi"; 
+console.log(x);
 
 // 7.) Console log a variable assigned to the sum of two numbers
 // your code here
-console.log('answer 8');
+var y = 3 + 5;
+console.log(y);
 
 // 8.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
-//    console.log('It was not..')
-//}
+if (8 > 7) {
+    console.log('It was!')
+} else {
+    console.log('It was not..')
+}
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-let answer = 3 * 3 + 1 / 8 + 12 / 2;
+let answer = (3 * 3 + 1) / ((8 + 12) / 2);
 console.log(answer == 1); // This should log true
